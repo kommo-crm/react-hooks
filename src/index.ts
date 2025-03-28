28670673546f5c0442b90227eb05155281071d77
Igor Nerusin <ithater.iwnl@gmail.com>
2025-03-28 12:06:27 +0300

    feat: add licence, github templates & reexport file.

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { useConst } from './useConst';
+export { useDebounce } from './useDebounce';
+export { useDidUpdateEffect } from './useDidUpdateEffect';
+export { useIsComponentMounted } from './useIsComponentMounted';
+export { useKeyboardListNavigation } from './useKeyboardListNavigation';
+export { useOnOutsideClick } from './useOnOutsideClick';
+export { useDeepCompareEffect } from './useDeepCompareEffect';
